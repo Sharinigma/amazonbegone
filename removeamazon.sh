fa57d17fef2922da996aaf351ed63e745e2edf44
Dylan Baugher <dylanbaughercontact@gmail.com>
2017-08-23 05:44:06 -0500

    Add files via upload

---
+++ removeamazon.sh
@@ -0,0 +1,7 @@
+#To run this in the terminal first type without parentheses:
+#(chmod +x removeamazon.sh) then all you have to do is type:
+#(./removeamazon.sh) have fun.
+sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
+
